d6a592cdec1bb9c2b8189b7d52094e1dd8d67798
tisukRoman <tisukroman@gmail.com>
2020-12-23 21:52:41 +0200

    Pets

---
+++ script.js
@@ -0,0 +1,42 @@
+let menuDisp = document.querySelector('.menu_display');
+let html = document.querySelector('html');
+
+let flag = false;
+function myFunction(x) {
+    x.classList.toggle("change");
+
+    if(flag == false){
+    html.style.overflowY = 'hidden';
+    menuDisp.style.height = 100 + '%';
+    flag = true;
+    }else{
+        html.style.overflowY = 'auto';
+        menuDisp.style.height = 0 + '%';
+        flag = false;
+    }
+}
+
+
+let arrowR = document.querySelector('.right');
+let arrowL = document.querySelector('.left');
+let panel = document.querySelectorAll('.card'); //массив карточек
+
+let move = 0;
+panel[0].style.left = 0 +'%'
+arrowL.addEventListener('click', function(){
+    if(panel[0].style.left >= 0 +'%')return true;
+    move += 30;
+    panel.forEach(function(elem){
+        elem.style.left = move + '%';
+    }); 
+});
+arrowR.addEventListener('click', function(){
+    if(panel[0].style.left < -120 +'%')return true;
+    move -= 30;
+    panel.forEach(function(elem){
+        elem.style.left = move + '%';
+    });
+});
+
+
+
