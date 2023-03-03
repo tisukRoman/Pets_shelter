86caa77dd08eaab29f59bf65e130e10b46255c40
tisukRoman <tisukroman@gmail.com>
2023-03-03 14:22:18 +0200

    added swiper for slider

--- script.js
+++ script.js
@@ -3,7 +3,48 @@ let html = document.querySelector("html");
 
 AOS.init();
 
+const swiper = new Swiper(".swiper", {
+  centeredSlides: true,
+  centerInsufficientSlides: true,
+  centeredSlidesBounds: true,
+  direction: "horizontal",
+  autoHeight: true,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 100,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 80,
+    },
+    940: {
+      slidesPerView: 3,
+      spaceBetween: 80,
+    },
+  },
+});
+
 let flag = false;
+
 function myFunction(x) {
   x.classList.toggle("change");
 
@@ -17,24 +58,3 @@ function myFunction(x) {
     flag = false;
   }
 }
-
-let arrowR = document.querySelector(".right");
-let arrowL = document.querySelector(".left");
-let panel = document.querySelectorAll(".card"); //массив карточек
-
-let move = 0;
-panel[0].style.left = 0 + "%";
-arrowL.addEventListener("click", function () {
-  if (panel[0].style.left >= 0 + "%") return true;
-  move += 30;
-  panel.forEach(function (elem) {
-    elem.style.left = move + "%";
-  });
-});
-arrowR.addEventListener("click", function () {
-  if (panel[0].style.left < -120 + "%") return true;
-  move -= 30;
-  panel.forEach(function (elem) {
-    elem.style.left = move + "%";
-  });
-});
